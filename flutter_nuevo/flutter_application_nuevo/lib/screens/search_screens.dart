122191a7fbe5ecb7de04460449667b55b2fa30af
Yulieth_Vanegas <yfacundo2731@gmail.com>
2023-10-05 15:54:12 -0500

    fEXXO

--- flutter_nuevo/flutter_application_nuevo/lib/screens/search_screens.dart
+++ flutter_nuevo/flutter_application_nuevo/lib/screens/search_screens.dart
@@ -9,9 +9,21 @@ class SearchScreens extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Buscador..."),
       ),
-        body :const Center(
-          child: Text("Pantalla de Busqueda"),
-        )
+        body : Center(
+      child: Column(mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const SelectionContainer.disabled(child: Text('BUSCADOR',style: TextStyle(fontSize: 28, color: Colors.green ,fontWeight: FontWeight.bold,fontStyle: FontStyle.italic))),
+         const SizedBox(height: 20,),
+          Image.asset("../assets/images/buscador.jpg",width: 500,height: 300,),
+          
+           const  Text(
+          "El cantautor, rapero y productor colombiano de reggaetón y cumbia Feid canta con un estilo reconocible al instante. Ha compuesto y grabado con algunas de las mayores estrellas del género, como Reykon, Nicky Jam y J Balvin.       |Feid, de nombre real Salomón Villada Hoyos, nació en Medellín (Colombia) el 19 de agosto de 1992, su nombre artístico hace referencia a la creencia en su talento; la palabra se traduce como “fe sana” para el cantante, que es fe en Dios y en sí mismo.", 
+           textAlign: TextAlign.center,)
+        
+
+      ])
+    ),
     );
   }
 }
+
